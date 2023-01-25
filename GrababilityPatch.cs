9295ba54066feafc7bb36a97f85b8cf9f2675044
evsey9 <evseyru1@mail.ru>
2023-01-25 10:10:51 +0300

    Add project files.

---
+++ GrababilityPatch.cs
@@ -0,0 +1,36 @@
+﻿namespace MoreGrabsMod
+{
+    class GrababilityPatch
+    {
+        private static bool Player_IsCreatureLegalToHoldWithoutStun(On.Player.orig_IsCreatureLegalToHoldWithoutStun orig, Player self, Creature testCreature)
+        {
+            orig(self, testCreature);
+            return true;
+        }
+        private static Player.ObjectGrabability Player_Grabability(On.Player.orig_Grabability orig, Player self, PhysicalObject testObj)
+        {
+            Player.ObjectGrabability origGrabability = orig(self, testObj);
+            if (origGrabability != Player.ObjectGrabability.CantGrab)
+            {
+                return origGrabability;
+            }
+            Player player = testObj as Player;
+            if (player != null)
+            {
+                if (player != self)
+                    return Player.ObjectGrabability.OneHand;
+                else
+                {
+                    return Player.ObjectGrabability.CantGrab;
+                }
+            }
+
+            return Player.ObjectGrabability.OneHand;
+        }
+        public static void Patch()
+        {
+            On.Player.Grabability += Player_Grabability;
+            On.Player.IsCreatureLegalToHoldWithoutStun += Player_IsCreatureLegalToHoldWithoutStun;
+        }
+    }
+}
---
+++ MoreGrabsMod.cs
@@ -0,0 +1,13 @@
+﻿using BepInEx;
+
+namespace MoreGrabsMod
+{
+    [BepInPlugin("drwoof.moregrabsmod", "More Grabs Mod", "1.1.1")]
+    public class MoreGrabsMod : BaseUnityPlugin
+    {
+        public MoreGrabsMod()
+        {
+            GrababilityPatch.Patch();
+        }
+    }
+}
