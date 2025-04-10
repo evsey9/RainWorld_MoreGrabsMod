81247d1209b3a7d0a00e7eb5b62212def13bb7c0
evsey9 <evseyru1@mail.ru>
2025-04-11 02:00:50 +0500

    bump version number with Watcher release, convert spaces to tabs

--- GrababilityPatch.cs
+++ GrababilityPatch.cs
@@ -1,36 +1,36 @@
 ﻿namespace MoreGrabs
 {
-    class GrababilityPatch
-    {
-        private static bool Player_IsCreatureLegalToHoldWithoutStun(On.Player.orig_IsCreatureLegalToHoldWithoutStun orig, Player self, Creature testCreature)
-        {
-            orig(self, testCreature);
-            return true;
-        }
-        private static Player.ObjectGrabability Player_Grabability(On.Player.orig_Grabability orig, Player self, PhysicalObject testObj)
-        {
-            Player.ObjectGrabability origGrabability = orig(self, testObj);
-            if (origGrabability != Player.ObjectGrabability.CantGrab && !MoreGrabsOptions.allOneHand.Value)
-            {
-                return origGrabability;
-            }
-            Player player = testObj as Player;
-            if (player != null)
-            {
-                if (player != self)
-                    return Player.ObjectGrabability.OneHand;
-                else
-                {
-                    return Player.ObjectGrabability.CantGrab;
-                }
-            }
+	class GrababilityPatch
+	{
+		private static bool Player_IsCreatureLegalToHoldWithoutStun(On.Player.orig_IsCreatureLegalToHoldWithoutStun orig, Player self, Creature testCreature)
+		{
+			orig(self, testCreature);
+			return true;
+		}
+		private static Player.ObjectGrabability Player_Grabability(On.Player.orig_Grabability orig, Player self, PhysicalObject testObj)
+		{
+			Player.ObjectGrabability origGrabability = orig(self, testObj);
+			if (origGrabability != Player.ObjectGrabability.CantGrab && !MoreGrabsOptions.allOneHand.Value)
+			{
+				return origGrabability;
+			}
+			Player player = testObj as Player;
+			if (player != null)
+			{
+				if (player != self)
+					return Player.ObjectGrabability.OneHand;
+				else
+				{
+					return Player.ObjectGrabability.CantGrab;
+				}
+			}
 
-            return Player.ObjectGrabability.OneHand;
-        }
-        public static void Patch()
-        {
-            On.Player.Grabability += Player_Grabability;
-            On.Player.IsCreatureLegalToHoldWithoutStun += Player_IsCreatureLegalToHoldWithoutStun;
-        }
-    }
+			return Player.ObjectGrabability.OneHand;
+		}
+		public static void Patch()
+		{
+			On.Player.Grabability += Player_Grabability;
+			On.Player.IsCreatureLegalToHoldWithoutStun += Player_IsCreatureLegalToHoldWithoutStun;
+		}
+	}
 }
--- MoreGrabsMod.cs
+++ MoreGrabsMod.cs
@@ -12,16 +12,16 @@ using System.Security.Permissions;
 
 namespace MoreGrabs
 {
-    [BepInPlugin("drwoof.moregrabs", "More Grabs Mod", "1.1.1")]
-    public class MoreGrabsMod : BaseUnityPlugin
-    {
-        public MoreGrabsMod()
-        {
-            this._options = new MoreGrabsOptions(this, base.Logger);
-            GrababilityPatch.Patch();
-            On.RainWorld.OnModsInit += new On.RainWorld.hook_OnModsInit(this.RainWorldOnOnModsInit);
-        }
-        private readonly MoreGrabsOptions _options;
+	[BepInPlugin("drwoof.moregrabs", "More Grabs Mod", "1.3.0")]
+	public class MoreGrabsMod : BaseUnityPlugin
+	{
+		public MoreGrabsMod()
+		{
+			this._options = new MoreGrabsOptions(this, base.Logger);
+			GrababilityPatch.Patch();
+			On.RainWorld.OnModsInit += new On.RainWorld.hook_OnModsInit(this.RainWorldOnOnModsInit);
+		}
+		private readonly MoreGrabsOptions _options;
 		private void RainWorldOnOnModsInit(On.RainWorld.orig_OnModsInit orig, RainWorld self)
 		{
 			orig.Invoke(self);
